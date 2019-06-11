7b56d9d98399c9e203171c73f624f2c6d2870378
Jack Arthur Harrhy <me@jackharrhy.com>
2019-06-11 14:37:13 -0230

    Allow alt text to be customized via options

--- qrcode.js
+++ qrcode.js
@@ -373,7 +373,7 @@
 			this._oContext = this._elCanvas.getContext("2d");
 			this._bIsPainted = false;
 			this._elImage = document.createElement("img");
-			this._elImage.alt = "Scan me!";
+			this._elImage.alt = htOption.altText ? htOption.altText : "Scan me!";
 			this._elImage.style.display = "none";
 			this._el.appendChild(this._elImage);
 			this._bSupportDataURI = null;
